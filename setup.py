e513414cff0085192c250152af8a2c92b6e9ba26
Julian Rossbroich <rossbroich@gmail.com>
2020-06-10 15:22:56 +0200

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     tests_require=test_requirements,
     setup_requires=setup_requirements,
     url='https://github.com/JRBCH/spiffyplots',
-    version='0.3.2',
+    version='0.3.3',
     cmdclass={'install': PostInstallMoveFile, },
 
 )
