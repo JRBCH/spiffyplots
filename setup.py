96e011c12487b7e7e83be6d2fd0561bd248aee2f
Julian Rossbroich <rossbroich@gmail.com>
2020-06-12 12:53:12 +0200

    pre-commit formatting

--- setup.py
+++ setup.py
@@ -28,11 +28,14 @@ with open(os.path.join(root, "README.md"), "r", encoding="utf-8") as f:
 requirements = ["matplotlib", "wheel", "numpy"]
 
 # Test suite requirements
-test_requirements = ["matplotlib", "numpy", "coverage", "pytest", "pytest-cov"]
+test_requirements = ["coverage", "pytest", "pytest-cov"]
 
 # Setup requirements
 setup_requirements = ["matplotlib", "wheel", "pytest-runner"]
 
+extras = {
+    'test': test_requirements,
+}
 
 def install_styles():
     # Find all style files
@@ -59,35 +62,30 @@ class PostInstallMoveFile(install):
 
 
 setup(
+    name="spiffyplots",
+    url="https://github.com/JRBCH/spiffyplots",
+    version="0.4.2",
+
     author="Julian Rossbroich",
     author_email="julian.rossbroich@fmi.ch",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-    ],
+    license="GPL-3",
+
     description=(
         "A collection of matplotlib style sheets and plotting tools for"
         " publication-ready figures"
     ),
-    license="MIT",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    keywords=[
-        "matplotlib",
-        "matplotlib-style-sheets",
-        "data visualization",
-        "matplotlib-styles",
-    ],
-    name="spiffyplots",
+
     packages=["spiffyplots"],
     package_data={"spiffyplots": ["styles/*"]},
     include_package_data=True,
+
     test_suite="tests",
     install_requires=requirements,
     tests_require=test_requirements,
     setup_requires=setup_requirements,
-    url="https://github.com/JRBCH/spiffyplots",
-    version="0.4.2",
+    extras_require=extras,
+
     cmdclass={"install": PostInstallMoveFile,},
 )
