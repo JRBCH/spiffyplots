13826acbafcb43a40022dabe0b892c93d4986368
Julian Rossbroich <julian.rossbroich@fmi.ch>
2021-05-26 15:21:59 +0200

    Bump version: 0.5.2 → 0.6.0

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ class PostInstallMoveFile(install):
 setup(
     name="spiffyplots",
     url="https://github.com/JRBCH/spiffyplots",
-    version="0.5.2",
+    version="0.6.0",
     author="Julian Rossbroich",
     author_email="julian.rossbroich@fmi.ch",
     license="GPL-3",
